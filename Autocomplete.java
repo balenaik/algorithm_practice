e5a7924d3ac7ebdccc602ba793d4b00bf8b02b8b
Aika Yamada <balenaik@gmail.com>
2020-11-01 13:04:45 +0900

    Auto complete without 1 typo correct function

---
+++ Autocomplete.java
@@ -0,0 +1,110 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class Autocomplete {
+
+  public static void main(String[] args) {
+    final Scanner sc = new Scanner(System.in);
+
+    final int n = sc.nextInt();
+    List<String> dict = new ArrayList<>(n);
+    sc.nextLine();
+    for (int i = 0; i < n; ++i) {
+      dict.add(sc.nextLine());
+    }
+
+    final int q = sc.nextInt();
+    List<String> queries = new ArrayList<>(q);
+    sc.nextLine();
+    for (int i = 0; i < q; ++i) {
+      queries.add(sc.nextLine());
+    }
+
+    solve(dict, queries);
+  }
+
+
+  static private void solve(List<String> dict, List<String> queries) {
+    Trie trie = new Trie();
+    for (String word : dict) {
+      trie.insert(word);
+    }
+    for (String query : queries) {
+      trie.printAutoCompleteResults(query);
+    }
+  }
+
+}
+
+class TrieNode {
+  HashMap<Character, TrieNode> children = new HashMap<>();
+  boolean isWordEnd;
+}
+
+class Trie {
+  TrieNode root = new TrieNode();
+
+  // If not present, inserts key into trie.  If the 
+  // key is prefix of trie node, just marks leaf node
+  void insert(String key) {
+    TrieNode pointNode = root;
+    for (char character : key.toCharArray()) {
+      TrieNode nextNode;
+      if (pointNode.children.containsKey(character)) {
+        nextNode = pointNode.children.get(character);
+      } else {
+        nextNode = new TrieNode();
+        pointNode.children.put(character, nextNode);
+      }
+      pointNode = nextNode;
+    }
+    pointNode.isWordEnd = true;
+  }
+
+  void printAutoCompleteResults(String query) {
+    // Returns all the words in the trie whose common 
+    // prefix is the given key thus listing out all  
+    // the suggestions for autocomplete. 
+    TrieNode pointNode = root;
+    boolean notFound = false;
+    // String tempWord = "";
+    ArrayList<String> autoCompleteResults = new ArrayList<String>();
+
+    for (char character : query.toCharArray()) {
+      if (pointNode.children.get(character) == null) {
+        notFound = true;
+        break;
+      }
+      // tempWord += character;
+      pointNode = pointNode.children.get(character);
+    }
+
+    if (notFound == true) {
+      System.out.println("<no matches>");
+      return;
+    }
+
+    traverseTrie(autoCompleteResults, pointNode, query);
+    System.out.println(toPrintString(autoCompleteResults));
+    return;
+  }
+
+  void traverseTrie(ArrayList<String> autoCompleteResults, TrieNode node, String query) {
+    // Method to recursively traverse the trie
+    // and return a whole word.
+    if (node.isWordEnd) {
+      autoCompleteResults.add(query);
+    }
+    node.children.forEach((character, nextNode) -> {
+      traverseTrie(autoCompleteResults, nextNode, query + character);
+    });
+  }
+
+  String toPrintString(ArrayList<String> array) {
+    String arrayString = Arrays.toString(array.toArray());
+    return arrayString.substring(1, arrayString.length() - 1).replace(',', ' ');
+  }
+}
