64f994d22847197ace1b5c6ba3ef2b028711225d
Aika Yamada <balenaik@gmail.com>
2020-11-01 19:37:10 +0900

    Remove null

--- src/week2/Autocomplete.java
+++ src/week2/Autocomplete.java
@@ -4,7 +4,7 @@ import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Autocomplete {
 
@@ -36,6 +36,7 @@ public class Autocomplete {
     }
     for (String query : queries) {
       String[] result = trie.getAutoCompleteResults(query);
+      
       String printResult = result.length == 0 ? "<no matches>" : toPrintString(result);
       System.out.println(printResult);
     }
@@ -73,18 +74,20 @@ class Trie {
   }
   
   String[] getAutoCompleteResults(String query) {
-    HashSet<String> result = autoCompleteResults(query, null, true);
-    return result.toArray(new String[result.size()]);
+    TreeSet<String> result = autoCompleteResults(query, null, true);
+    int maxArraySize = 10;
+    int arraySize = result.size() < maxArraySize ? result.size() : maxArraySize;
+    return result.toArray(new String[arraySize]);
   }
 
-  HashSet<String> autoCompleteResults(String query, HashSet<String> results, boolean allowsTypo) {
+  TreeSet<String> autoCompleteResults(String query, TreeSet<String> results, boolean allowsTypo) {
     // Returns all the words in the trie whose common 
     // prefix is the given key thus listing out all  
     // the suggestions for autocomplete. 
     TrieNode pointNode = root;
     boolean notFound = false;
     if (results == null) {
-      results =  new HashSet<String>();
+      results =  new TreeSet<String>();
     }
 
     for (int i = 0; i < query.toCharArray().length; i++) {
