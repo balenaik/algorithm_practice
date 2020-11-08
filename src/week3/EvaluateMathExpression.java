bf2f051b5e76e06defc6e775c626f36b2e45d5fb
Aika Yamada <balenaik@gmail.com>
2020-11-08 20:44:44 +0900

    Add a question

---
+++ src/week3/EvaluateMathExpression.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class EvaluateMathExpression {
+    public static float evaluateMathExpression(String s) {
+    String[] s0 = s.split(" ");
+    if (s0.length != 3) {
+     throw new IllegalArgumentException("Must have 3 tokens separated by spaces: " + s);
+    }
+    // s1 is first number.
+    String s1 = s0[0];
+    // s2 is math operator (e.g. '+').
+    String s2 = s0[1];
+    // s3 is second number.
+    String s3 = s0[2];
+
+    // first number
+    int v1 = 0;
+    for (int i = 0; i < s1.length(); i++) {
+      char c = s1.charAt(i);
+      if (c < '0' || c > '9') {
+    	  throw new NumberFormatException("Can't convert character to digit: " + c);
+      }
+      int digit = c - '0';
+      v1 += digit * Math.pow(10, s1.length() - i - 1);
+    }
+
+    // second number
+    int v2 = 0;
+    for (int i = 0; i < s3.length(); i++) {
+      char c = s3.charAt(i);
+      if (c < '0' || c > '9') {
+    	  throw new NumberFormatException("Can't convert character to digit: " + c);
+      }
+      int digit = c - '0';
+      v2 += digit * Math.pow(10, s3.length() - i - 1);
+    }
+
+    if (s2.equals("+")) {
+      // Returns first number plus second number.
+      float result = v1 + v2;
+      return result;
+    } else if (s2.equals("-")) {
+      // Returns first number minus second number.
+      float result = v1 - v2;
+      return result;
+    }
+    else {
+      throw new NumberFormatException("Can't convert character an operator: +, -, /, *");
+    }
+  }
+  
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		String s = cin.nextLine();
+		cin.close();
+		try {
+			float result = evaluateMathExpression(s);	
+			System.out.println(result);
+		}
+		catch(NumberFormatException e) {
+			System.out.println("NumberFormatException");
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException");
+		}
+	}
+}
